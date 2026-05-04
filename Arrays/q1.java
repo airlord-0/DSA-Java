f910989863af8d7c25fff4eff1a0c8e1b3f758b7
Pavan Kumar BD <pavankumarbd1028@gmail.com>
2026-05-04 10:51:08 +0530

    added gitignroe file

---
+++ Arrays/q1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class q1 {
+   public static void main (String args[]) {
+    Scanner sc = new Scanner (System.in);
+    int n = sc.nextInt();
+    int marks[] = new int[n];
+    for (int i=0; i<n; i++) {
+        marks[i]=sc.nextInt();       
+    }
+    System.out.println("enter the number u wanna search index of : ");
+    int search = sc.nextInt();
+    
+    for (int j=0; j<n; j++) {
+        if (marks[j]==search) {
+            System.out.print("the number is idexed at : "+j);
+            
+        }
+        
+        
+    }
+
+   } 
+}
---
+++ Arrays/twodaraays.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class twodaraays {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int rows = sc.nextInt();
+        int cols= sc.nextInt();
+
+        // declare a 2d array now 
+        int [][] matrix = new int [rows] [cols];
+
+        // to take inpute use 2 loops with out loop controling rows
+        // and inner loop controlling cols such as for each row we take 
+        // n cols input 
+
+        for (int i=0; i<rows;i++) {
+            for (int j=0; j<cols; j++) {
+               // ask the user for the input 
+               System.out.println("enter the number of  : " + i + j);
+               
+               matrix[i][j]=sc.nextInt();
+               
+            }
+        }
+        System.out.println("enter the number u wanna search : ");
+        
+        int search =sc.nextInt();
+        for (int i=0; i<rows;i++) {
+            for (int j=0; j<cols; j++) {
+                System.out.println(matrix[i][j]+" ");
+                if (search==matrix[i][j]) {
+                    System.out.println("the searched value is indexed at : "+i+j);
+                    
+                }
+                
+
+            }
+            System.out.println("");
+            
+        }   
+
+    }
+}
